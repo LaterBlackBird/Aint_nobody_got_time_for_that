6492dd494d2ba117e941b4aa5fc758da4f8d8956
Seth <seth.holland@gmail.com>
2022-01-20 09:29:38 -0600

    function in place for selecting a meal plan

--- react-app/src/components/MealPlans/index.js
+++ react-app/src/components/MealPlans/index.js
@@ -7,21 +7,25 @@ import './meal_plans.css'
 
 function MealPlans() {
     const dispatch = useDispatch();
-    const user = useSelector(state => state.session.user)
-    const plans = useSelector(state => Object.values(state.mealPlans))
+    const user = useSelector(state => state.session.user);
+    const plans = useSelector(state => state.mealPlans);
+    const plansArray = Object.values(plans);
 
     useEffect(() => {
-        dispatch(getMealPlans(user.id))
-
+        dispatch(getMealPlans(user.id));
     }, [dispatch, user])
+
+    const selectMealPlan = (planId) => {
+        console.log('you selected ', plans[planId].name);
+    }
 
     return (
         <div id="meal_plan_container">
             <div id="meal_plan_header">
                 <p>MEAL PLANS</p>
             </div>
-            {plans.map(plan => (
-                <div className="meal_plan_card" key={plan.id}>
+            {plansArray.map(plan => (
+                <div className="meal_plan_card" key={plan.id} onClick={() => selectMealPlan(plan.id)}>
                     <p>{plan.name}</p>
                 </div>
             ))}
