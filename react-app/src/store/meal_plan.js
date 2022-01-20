8ebf5064a914aa86fa2cfb5b456310c9f23878d9
Seth <seth.holland@gmail.com>
2022-01-20 16:47:18 -0600

    state changes when adding plan, but does not update component

--- react-app/src/store/meal_plan.js
+++ react-app/src/store/meal_plan.js
@@ -16,7 +16,7 @@ export const addMealPlan = planInfo => async (dispatch) => {
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({newPlanName, userId})
+        body: JSON.stringify({ newPlanName, userId })
     });
     if (response.ok) {
         const plan = await response.json();
@@ -25,11 +25,15 @@ export const addMealPlan = planInfo => async (dispatch) => {
     }
 }
 
+export const selectThisPlan = planInfo => async (dispatch) => {
+    dispatch(selectedPlan(planInfo));
+}
 
 // Action types
 // To help prevent errors
 const GET_MEAL_PLANS = 'user/GET_MEAL_PLANS'
 const ADD_MEAL_PLANS = 'meal_plans/ADD_MEAL_PLANS'
+const SET_MEAL_PLAN = 'meal_plans/SET_MEAL_PLAN'
 
 
 // Actions
@@ -47,10 +51,17 @@ const createMealPlan = (newMealPlan) => {
     }
 }
 
+const selectedPlan = (plan) => {
+    return {
+        type: SET_MEAL_PLAN,
+        plan
+    }
+}
+
 
 // Reducer
 // Replace state with database information from thunk
-export default function mealPlanReducer(state = {}, action) {
+export default function mealPlanReducer(state = { all_plans: {} }, action) {
     switch (action.type) {
         case GET_MEAL_PLANS:
             // normalize data
@@ -59,12 +70,16 @@ export default function mealPlanReducer(state = {}, action) {
                 allMealPlans[mealPlan.id] = mealPlan;
             });
             return {
-                ...allMealPlans,
+                all_plans: { ...allMealPlans },
             };
         case ADD_MEAL_PLANS:
-            let addState = {...state};
-            addState[action.newMealPlan.id] = action.newMealPlan;
+            let addState = { ...state };
+            addState.all_plans[action.newMealPlan.id] = action.newMealPlan;
             return addState
+        case SET_MEAL_PLAN:
+            let setState = { ...state };
+            setState['selected'] = action.plan;
+            return setState
         default:
             return state;
     }
