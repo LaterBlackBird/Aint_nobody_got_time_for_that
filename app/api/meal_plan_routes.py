7dae1dfc30265b6c550db5fca44cbfa72648fce5
Seth <seth.holland@gmail.com>
2022-01-20 12:14:53 -0600

    user can now add a new meal plan that shows up on the list after creation

--- app/api/meal_plan_routes.py
+++ app/api/meal_plan_routes.py
@@ -1,16 +1,24 @@
 from flask import Blueprint, jsonify, session, request
-from app.models import User, db
-from app.forms import LoginForm
-from app.forms import SignUpForm
-from flask_login import current_user, login_user, logout_user, login_required
+from flask_login import login_required
+from app.models import Meal_Plan, db
+from app.forms import NewMealPlanForm
 
 meal_routes = Blueprint('meal_plans', __name__)
 
-@meal_routes.route('/')
+@meal_routes.route('', methods = ['POST'])
 @login_required
-def get_meal_plans():
-    meal_plans = Meal_Plan.query.filter(Meal_Plan.user_id == userId).all()
-    return {'meal_plans': [user.to_dict() for user in users]}
+def add_meal_plan():
+    form = NewMealPlanForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+
+    if form.validate_on_submit():
+        new_meal_plan = Meal_Plan(
+            name=form.data['newPlanName'],
+            user_id=form.data['userId']
+        )
+        db.session.add(new_meal_plan)
+        db.session.commit()
+    return new_meal_plan.to_dict()
 
 
 @meal_routes.route('/<int:id>')
--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -1,2 +1,3 @@
 from .login_form import LoginForm
 from .signup_form import SignUpForm
+from .new_meal_plan_form import NewMealPlanForm
