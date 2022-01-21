7f845fe4740b7230c241308d26a118d16d54a302
Seth <seth.holland@gmail.com>
2022-01-21 17:31:45 -0600

    clicking on the name of a dialy schedule opens the edit form

--- react-app/src/components/DailyScheduleCard/index.js
+++ react-app/src/components/DailyScheduleCard/index.js
@@ -1,13 +1,55 @@
-import React from "react";
-import { useSelector } from 'react-redux';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import './daily_schedule.css'
 
 function DailyScheduleCard({ dailySchedule }) {
+    const dispatch = useDispatch();
+    const [editDayNameFormVisibility, setEditDayNameFormVisibility] = useState(false)
+    const [editedDayName, setEditedDayName] = useState('')
+    const [errors, setErrors] = useState([]);
+
+
+    //edit the daily schedule name
+    const editDay = async (e) => {
+        e.preventDefault();
+        setErrors([]);
+
+        if (editedDayName.length < 1) {
+            setErrors(['Name Cannot Be Empty'])
+        } else {
+            // dispatch(editDailySchedule(selectedPlan.id, editedPlanName))
+            setEditDayNameFormVisibility(false);
+            setEditedDayName('')
+        }
+    }
+
+    let editDayForm;
+    if (editDayNameFormVisibility) {
+        editDayForm = (
+            <form onSubmit={editDay} className='daily_schedule_header flex_col_center'>
+                <input
+                    name='editDayName'
+                    ref={(input) => { input && input.focus() }}
+                    type='text'
+                    placeholder={errors.length ? errors[0] : `${dailySchedule.name}`}
+                    value={editedDayName}
+                    onChange={(e) => setEditedDayName(e.target.value)}
+                    className="edit_daily_schedule_header flex_col_center"
+                />
+                <p onClick={() => setEditDayNameFormVisibility(false)}>Cancel</p>
+            </form>
+        )
+    }
+
 
     return (
         <div className="daily_schedule_card flex_col_center">
+            <i className="fas fa-times daily_schedule_delete"></i>
             <div className='daily_schedule_header flex_col_center'>
-                <p>{dailySchedule.name}</p>
+                {editDayNameFormVisibility ? editDayForm :
+                    <p onClick={() => setEditDayNameFormVisibility(true)}>{dailySchedule.name}</p>
+                }
+
             </div>
         </div>
     );
