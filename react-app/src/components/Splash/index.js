d77afc5e95c02c9c15432776ccc7f415060c0a73
Seth <seth.holland@gmail.com>
2022-01-22 17:28:05 -0600

    hero bar styled

--- react-app/src/components/Splash/index.js
+++ react-app/src/components/Splash/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import eating from '../images/undraw_eating_together_re_ux62.svg'
-import logo from '../images/logo.png'
+import logo from '../images/logo.svg'
 import './splash.css'
 
 function Splash() {
@@ -9,7 +10,7 @@ function Splash() {
             <div id='hero_bar_branding' className='flex_col_center'>
                 <div id='brand'>
                     <p>AINT NOBODY<br /> GOT TIME<br /> FOR THAT</p>
-                    <img src={logo} alt="logo"/>
+                        <img src={logo} alt="logo" />
                 </div>
                 <div className='copy'>
                     <span>Eating a healthy diet is more important that ever, but it doesn’t happen by accident. <br />
@@ -19,9 +20,9 @@ function Splash() {
                         Planning made easy and fast. Spend less time planning your <br />
                         meals and more time enjoying them with the people you care about.</span>
                 </div>
-                <button>Get Started For Free</button>
+                <button><Link to='/sign-up' className='sign_up'> Get Started For Free </Link></button>
             </div>
-            <img src={eating} alt="people eating at a table" />
+            <img src={eating} alt="people eating at a table" id='hero_image'/>
         </div >);
 }
 
