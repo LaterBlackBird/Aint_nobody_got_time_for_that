1d0795e72afa7ed23ca215745df22b33d3b00cf9
Seth <seth.holland@gmail.com>
2022-01-25 15:03:11 -0600

    search bar styled

--- frontend/src/components/RecipeSearch/index.js
+++ frontend/src/components/RecipeSearch/index.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
 import './recipeSearch.css'
 
 function RecipeSearch({ setShowRecipeSearch, dayId }) {
+    const [searchText, setSearchText] = useState('');
+    const [errors, setErrors] = useState([]);
+
+    useEffect(() => {
+        
+    })
+
+    const search = () => { }
 
     return (
-            <div className="search_container flex_col_center">
-                <h2>Search For A Recipe</h2>
-                <form action="">
-                    <input type="search" placeholder='Search by name or tag'/>
-                </form>
-            </div>
+        <div className="search_container">
+            <h2>Search For A Recipe</h2>
+            <form onSubmit={(e)=> e.preventDefault()}>
+                <input
+                    name='searchRecipes'
+                    ref={(input) => { input && input.focus() }}
+                    type='search'
+                    placeholder={errors.length ? errors[0] : `Search by name or tag`}
+                    value={searchText}
+                    onChange={(e) => setSearchText(e.target.value)}
+                    className="search_input"
+                />
+            </form>
+
+        </div>
     );
 }
 
