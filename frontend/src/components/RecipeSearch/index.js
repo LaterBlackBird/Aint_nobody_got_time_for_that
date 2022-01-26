f28bbce0cec177e4d1822451a24905c605ee2efc
Seth <seth.holland@gmail.com>
2022-01-25 18:52:58 -0600

    search results show underneath search bar

--- frontend/src/components/RecipeSearch/index.js
+++ frontend/src/components/RecipeSearch/index.js
@@ -7,17 +7,22 @@ import './recipeSearch.css'
 function RecipeSearch({ dayId }) {
     const dispatch = useDispatch();
     const [searchText, setSearchText] = useState('');
-    const searchResultsArr = useSelector(state => Object.values(state.recipes.searchResults))
+    const searchResults = useSelector(state => state.recipes.searchResults);
+    let searchResultsArray = [];
 
     useEffect(() => {
         if (searchText) dispatch(searchRecipes(searchText));
-    },[dispatch, searchText])
+    }, [dispatch, searchText])
 
+
+    if (searchResults) {
+        searchResultsArray = Object.values(searchResults);
+    }
 
     return (
         <div className="search_container">
             <h2>Search For A Recipe</h2>
-            <form onSubmit={(e)=> e.preventDefault()}>
+            <form onSubmit={(e) => e.preventDefault()}>
                 <input
                     name='searchRecipes'
                     ref={(input) => { input && input.focus() }}
@@ -29,11 +34,11 @@ function RecipeSearch({ dayId }) {
                 />
             </form>
             <div className="search_results_container">
-                {/* {searchResultsArr &&
-                    searchResultsArr.map(result => {
+                {searchResultsArray &&
+                    searchResultsArray.map(result => (
                         <SearchResultCard key={result.id} recipe={result} dayId={dayId}/>
-                    })
-                } */}
+                    ))
+                }
             </div>
 
         </div>
