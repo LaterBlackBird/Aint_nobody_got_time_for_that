a61ff2e29db3a9a5bc790ae3ff1995a718221c96
Seth <seth.holland@gmail.com>
2022-01-26 15:53:27 -0600

    finished about section

--- frontend/src/components/Splash/index.js
+++ frontend/src/components/Splash/index.js
@@ -6,6 +6,7 @@ import schedule from '../images/undraw_date_picker_re_r0p8.svg'
 import meal_plan_preview from '../images/meal_plan_preview.png'
 import daily_schedule_preview from '../images/daily_schedule_preview.png'
 import shopping from '../images/undraw_empty_cart_co35.svg'
+import me from '../images/Seth_2.jpg'
 import './splash.css'
 
 function Splash() {
@@ -56,7 +57,34 @@ function Splash() {
             </div>
             <div id="about">
                 <h2>About</h2>
-                <h3>Seth Holland</h3>
+                <div className="info">
+                    <img id='selfie' src="https://avatars.githubusercontent.com/u/14840521?v=4" alt="" />
+                    <div className='links flex_col_center'>
+                        <h3 className='name'>Seth Holland</h3>
+                        <div className="author_links">
+                            <a href='https://www.linkedin.com/in/seth-holland/' target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin-in splash_text"></i></a>
+                            <a href='https://github.com/LaterBlackBird' target="_blank" rel="noopener noreferrer"><i className="fab fa-github splash_text"></i></a>
+                            <a href='https://angel.co/u/seth-holland' target="_blank" rel="noopener noreferrer"><i className="fab fa-angellist splash_text"></i></a>
+
+                        </div>
+                    </div>
+                </div>
+                <div id='built_with'>
+                    <h3>Built With:</h3>
+                    <div>
+                        <img className='badge' src={`https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black`} alt="JavaScript" />
+                        <img className='badge' src={`https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54`} alt="Python" />
+                        <img className='badge' src={`https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white`} alt="HTML5" />
+                        <img className='badge' src={`https://img.shields.io/badge/CSS-239120?&style=for-the-badge&logo=css3&logoColor=white`} alt="CSS3" />
+                        <img className='badge' src={`https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white`} alt="NodeJS" />
+                        <img className='badge' src={`https://img.shields.io/badge/PostgreSQL-316192?style=for-the-badge&logo=postgresql&logoColor=white`} alt="PostgreSQL" />
+                        <img className='badge' src={`https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB`} alt="React" />
+                        <img className='badge' src={`https://img.shields.io/badge/Redux-593D88?style=for-the-badge&logo=redux&logoColor=white`} alt="Redux" />
+                        <img className='badge' src={`https://img.shields.io/badge/Flask-000000?style=for-the-badge&logo=flask&logoColor=white`} alt="Flask" />
+                        <img className='badge' src={`https://img.shields.io/badge/Heroku-430098?style=for-the-badge&logo=heroku&logoColor=white`} alt="Heroku" />
+                        <img className='badge' src={`https://img.shields.io/badge/docker-%230db7ed.svg?style=for-the-badge&logo=docker&logoColor=white`} alt="Docker" />
+                    </div>
+                </div>
             </div>
         </>
     );
