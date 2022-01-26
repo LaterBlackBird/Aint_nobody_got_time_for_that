e6a81f5c09dbbc3dbb1ef8ed8eb0a2094831c635
Seth <seth.holland@gmail.com>
2022-01-26 09:56:07 -0600

    additional search result styling complete

--- frontend/src/components/SearchResultCard/index.js
+++ frontend/src/components/SearchResultCard/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import './searchResultCard.css'
+
 
 function SearchResultCard({recipe, dayId}) {
+  const addRecipe = () => {
+    console.log('click')
+  }
+
   return (
     <div className="search_result_card">
         <p>{recipe.name}</p>
+        <p className='plus' onClick={() => addRecipe()}>+</p>
     </div>
     );
 }
